7f733a720f92da3a289789015ec8a6ab8c43c72a
Mehabagadia <bagadiameha@gmail.com>
2023-06-26 08:30:13 +0530

    new

--- file1.py
+++ file1.py
@@ -7,7 +7,7 @@ fathersName: Label(top, text="Print model:").place(x=20,y=80)
 motherName: Label(top , text="company name:").place(x=20,y=120)
     
 def CallBack():
-    tkinter.messagebox.showinfo("Message", "Your data is submitted")
+    tkinter.messagebox.showinfo("Message", "Your data is submitted. ")
 submit: Button(top, text="SUBMIT",command=CallBack).place(x=30,y=240)
 e1=Entry(top).place(x=120,y=40)
 e2=Entry(top).place(x=120,y=80)
